f2f269369bdd6d9525091396f40b9e0f73055bd0
Roland <rdorde@gmail.com>
2017-05-02 00:33:53 +0200

    Update array-cardio.js

--- array-cardio.js
+++ array-cardio.js
@@ -62,3 +62,43 @@ const counted = data.reduce((obj, x) => {
   return obj
 }, {})
 // console.log(counted)
+
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const people2 = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const adults = people2.some(x => (new Date()).getFullYear() - x.year > 18)
+// console.log(adults)
+
+// Array.prototype.every() // is everyone 19 or older?
+const adultsAll = people2.every(x => (new Date()).getFullYear() - x.year > 18)
+// console.log(adultsAll) 
+
+
+// Array.prototype.find()
+// Find the comment with the ID of 823423
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+const found = comments.find(x => x.id === 823423)
+// console.log(found)
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+const foundIndex = comments.findIndex(x => x.id === 823423)
+// console.log(foundIndex)
+
+// Delete the comment with the ID of 823423
+comments.splice(foundIndex, 1)
+// console.table(comments) 
